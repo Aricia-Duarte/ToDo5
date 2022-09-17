e32f2cf9bc0e7bc1c4e29c06138ede26ba12e132
Aricia-Duarte <ariciapjd@hotmail.com>
2022-09-16 21:14:02 -0300

    inclusao do DML-Inserts

---
+++ DML-Inserts.sql
@@ -0,0 +1,18 @@
+use vitrine_aricia;
+
+insert into produto(nomeProduto,valorProduto,descricaoProduto,estoqueProduto,departamentoProduto,cartegoriaProduto) values ("Camisa Xadrez",125.53,"Camisa flanelada para toda a familia",10,"Feminino","Camisas e Camisetas"),
+("Camiseta Croped",49.90,"Camiseta Croped de Algodão Egipcio",1,"Feminino","Camisas e Camisetas"),
+("Body Infantil",69.90,"Body Infantil para bebes de 6 a 12 meses",20,"Infantil","Body"),
+("Pijama Infantil Bobs",75.00,"Pijama de calça e camiseta de Manga Longa",2,"Infantil","Moda Intima"),
+("Calça de Sarja",125.50,"Calça Infantil de Menino",7,"Infantil","Calças e Shorts"),
+("Vestido de Verão Florido",45.75,"Vestido de Malha Florido",35,"Feminino","Vestidos e Saias"),
+("Blusa de Trico",32.55,"Blusa de Trico Feminina",25,"Feminino","Blusas e Sueter"),
+("Saia Longa de Croche",255.00,"Saia Longa de Croche feita com linhas sustentaveis",2,"Feminino","Vestidos e Saias"),
+("Camisa Social Branca",200.00,"Camisa Social Branca em Algodao Egipcio",10,"Masculino","Camisas e Camisetas"),
+("Calça Social",235.50,"Calça Social de Brim",2,"Masculino","Calças e Shorts"),
+("Roupao StarWars",119.90,"Roupão Flanelado estampa StarWars",12,"Masculino","Moda Intima"),
+("Pijama Masculino Manga Curta",89.90,"Pijama Masculino de Shorts e Manga Curta",2,"Masculino","Moda Intima"),
+("Biquini Cavado Rosa",98.00,"Biquini Cavado na Cor Rosa",5,"Feminino","Moda Intima"),
+("Short Masculino de Banho",55.00,"Short Masculino em tecido Impermeavel",25,"Masculino","Calças e Shorts"),
+("Camisa Social Branca",102.10,"Camisa Social em Tecido que não Amassa",3,"Feminino","Camisas e Camisetas")
+;
